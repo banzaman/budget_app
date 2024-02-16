a927a5ebeb39bad8abec28c059d1b508e48411c6
MARK RUBANZA <markrubanza10@gmail.com>
2024-02-16 21:23:32 +0300

    Correct linter errors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,33 +1,33 @@
 class HomeController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def index
-      @categories = Group.includes(:entities).all
-    end
+  def index
+    @categories = Group.includes(:entities).all
+  end
 
-    def show
-      @category = Group.includes(:entities).find(params[:category_id])
-      @transactions = @category.entities.order('created_at DESC')
-    end
+  def show
+    @category = Group.includes(:entities).find(params[:category_id])
+    @transactions = @category.entities.order('created_at DESC')
+  end
 
-    def new
-      @category = Group.new
-    end
+  def new
+    @category = Group.new
+  end
 
-    def create
-      @category = Group.new(category_params)
-      @category.author_id = current_user.id
-      if @category.save
-        redirect_to home_index_path
-      else
-        flash[:error] = @category.errors.full_messages.to_sentence
-        render :new, status: :unprocessable_entity
-      end
-    end
-
-    private
-
-    def category_params
-      params.require(:group).permit(:name, :icon)
+  def create
+    @category = Group.new(category_params)
+    @category.author_id = current_user.id
+    if @category.save
+      redirect_to home_index_path
+    else
+      flash[:error] = @category.errors.full_messages.to_sentence
+      render :new, status: :unprocessable_entity
     end
   end
+
+  private
+
+  def category_params
+    params.require(:group).permit(:name, :icon)
+  end
+end
