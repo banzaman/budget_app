d09d57cbdd4c52337c13df0f5037135b36bffabb
MARK RUBANZA <markrubanza10@gmail.com>
2024-02-25 08:56:38 +0300

    correct linter errors

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,36 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe 'categories/index.html.erb', type: :view do
-  let(:category1) { create(:category, name: 'Category 1', icon: 'icon1.png') }
-  let(:category2) { create(:category, name: 'Category 2', icon: 'icon2.png') }
+RSpec.describe 'home/index.html.erb', type: :view do
+  describe 'renders correctly' do
+    it 'displays categories correctly' do
+      categories = [
+        Group.new(id: 1, name: 'Category 1', icon: 'icon1'),
+        Group.new(id: 2, name: 'Category 2', icon: 'icon2')
+      ]
 
-  before do
-    assign(:categories, [category1, category2])
-    render
-  end
+      assign(:categories, categories)
 
-  it 'displays the title' do
-    expect(rendered).to have_selector('.title h3', text: 'CATEGORIES')
-  end
+      render
 
-  it 'displays each category with its icon, name, and total amount' do
-    expect(rendered).to have_selector('.categories-container .card', count: 2)
+      expect(rendered).to have_css('.title h3', text: 'CATEGORIES')
 
-    expect(rendered).to have_selector('.categories-container .card:nth-of-type(1) .card-info p', text: 'Category 1')
-    expect(rendered).to have_selector('.categories-container .card:nth-of-type(1) .card-info .category-icon[src="icon1.png"]')
-    expect(rendered).to have_selector('.categories-container .card:nth-of-type(1)', text: '$0')
-
-    expect(rendered).to have_selector('.categories-container .card:nth-of-type(2) .card-info p', text: 'Category 2')
-    expect(rendered).to have_selector('.categories-container .card:nth-of-type(2) .card-info .category-icon[src="icon2.png"]')
-    expect(rendered).to have_selector('.categories-container .card:nth-of-type(2)', text: '$0')
-  end
-
-  it 'displays a link to add a new category' do
-    expect(rendered).to have_link('ADD CATEGORY', href: home_new_path, class: 'category-new-btn')
-  end
-
-  it 'links each category card to its show page' do
-    expect(rendered).to have_link(href: home_show_path(category1.id), class: 'card-link')
-    expect(rendered).to have_link(href: home_show_path(category2.id), class: 'card-link')
+      categories.each do |category|
+        expect(rendered).to have_link(category.name, href: home_show_path(category.id), class: 'card-link')
+        expect(rendered).to have_css('.card-category .card-info .category-icon', text: category.icon)
+        expect(rendered).to have_css('.card-category .card-info p', text: category.name)
+        expect(rendered).to have_css('.card-category p', text: "$#{category.entities.sum(:amount)}")
+      end
+    end
   end
 end
