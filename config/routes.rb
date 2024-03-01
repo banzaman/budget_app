fe35078b695c1534425d323bc1a3311a373fc8b9
MARK RUBANZA <markrubanza10@gmail.com>
2024-03-02 01:38:47 +0300

    give the pages full height

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root 'splash#index'
-
   get 'home/index', as: 'home_index'
   get 'home/index/new', to: 'home#new', as: 'home_new'
+  get 'home/index/new2', to: 'home#new2', as: 'home_new2'
   get 'home/index/:category_id', to: 'home#show', as: 'home_show'
   get 'home/index/:category_id/new', to: 'transactions#new', as: 'transactions_new'
   post 'home/index/:category_id/create', to: 'transactions#create', as: 'transactions_create'
-
   post 'home/index/create', to: 'home#create', as: 'home_create'
-
 end
